95b81afc4b0398ddf433942a7795bd321e55434d
Toiha ISSIHACA <toiha@etic.yt>
2017-04-26 15:06:42 +0300

    Clean code

--- Resources/public/js/jquery.eticMenuPickerApi.js
+++ Resources/public/js/jquery.eticMenuPickerApi.js
@@ -1,6 +1,4 @@
 (function($) {
-    console.log('toiha');
-
     /**
      * Widget chargé de récupérer des items à ajouter dans le menu
      */
@@ -92,8 +90,6 @@
                     $(obj.contenus).each($.proxy(disaply_method, this));
                     
                     this.element.find('.panel-controls .fa-spinner').remove();
-//                    this.element.find('.content-picker').scroller("reset");
-//                    this.element.find('.content-picker').scroller().scroller("scroll", 0, 0);
                 },
                 error: function(){
                     this.element.find('.panel-controls .fa-spinner').remove();
