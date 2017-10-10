8921d43f035d37732068add023636127c406dd58
Johan Desmyter <johan@etic.yt>
2017-10-10 16:21:47 +0300

    ajout widget alert

---
+++ Widget/CMS/AlertWidget.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Kalamu\CmsAdminBundle\Widget\CMS;
+
+use Kalamu\CmsAdminBundle\Form\Type\WysiwygType;
+use Kalamu\DashboardBundle\Model\AbstractConfigurableElement;
+use Symfony\Bundle\TwigBundle\TwigEngine;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Form;
+
+/**
+ * Widget affichant un block alert bootstrap
+ */
+class AlertWidget extends AbstractConfigurableElement
+{
+
+    protected $template;
+
+    public function __construct($template) {
+        $this->template = $template;
+    }
+
+    public function getTitle() {
+        return 'cms.alert.title';
+    }
+
+    public function getDescription() {
+        return 'cms.alert.description';
+    }
+
+    public function getForm(Form $form){
+        $form->add('type', ChoiceType::class, array(
+            'choices' => ['Vert' => 'success', 'Orange' => 'warning', 'Rouge' => 'danger'],
+            'choices_as_values' => true,
+            'label' => "Type d'affichage",
+            'required' => true
+        ));
+        $form->add('contenu', WysiwygType::class, [
+            'label' => 'Contenu',
+            'required' => true
+        ]);
+
+        return $form;
+    }
+
+    /**
+     * Génère le widget qui doit être affiché dans le tableau de bord
+     * @return string
+     */
+    public function render(TwigEngine $templating, $intention = 'edit'){
+        $this->parameters['intention'] = $intention;
+
+        return $templating->render($this->template,$this->parameters);
+    }
+
+}
