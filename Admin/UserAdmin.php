ba837a0cbc15ee206a23fee68ca07331f2f64325
Johan Desmyter <johan@etic.yt>
2019-08-08 10:02:35 +0300

    ensure that there is always at least one super admin

--- Admin/UserAdmin.php
+++ Admin/UserAdmin.php
@@ -11,13 +11,20 @@
 
 namespace Kalamu\CmsAdminBundle\Admin;
 
+use Doctrine\ORM\EntityManagerInterface;
 use FOS\UserBundle\Doctrine\UserManager;
+use Kalamu\CmsAdminBundle\Entity\User;
 use Sonata\AdminBundle\Admin\AbstractAdmin;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Form\FormMapper;
 use Sonata\AdminBundle\Show\ShowMapper;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Validator\Constraints\Callback;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
 
 class UserAdmin extends AbstractAdmin
 {
@@ -27,9 +34,19 @@ class UserAdmin extends AbstractAdmin
      */
     protected $fosUserManager;
 
+    /**
+     * @var EntityManagerInterface
+     */
+    protected $doctrine;
+
     public function setFosUserManager(UserManager $fosUserManager)
     {
         $this->fosUserManager = $fosUserManager;
+    }
+
+    public function setEntityManager(EntityManagerInterface $doctrine)
+    {
+        $this->doctrine = $doctrine;
     }
 
     public function preUpdate($object) {
@@ -90,11 +107,57 @@ class UserAdmin extends AbstractAdmin
             ->add('email')
             ->add('enabled')
             ->add('plain_password', PasswordType::class, [
-                'required' => false
+                'required' => $this->getSubject()->getId() ? false : true
             ])
-            ->add('super_admin')
+            ->add('super_admin', CheckboxType::class, [
+                'data' => $this->getSubject()->hasRole(User::ROLE_SUPER_ADMIN),
+                'mapped' => false,
+                'required' => false,
+                'constraints' => new Callback(['callback' => [$this, 'checkSuperAdminSafe']])
+            ])
             ->add('groups')
         ;
+
+        if($this->isGranted(User::ROLE_SUPER_ADMIN)){
+            $formMapper->getFormBuilder()->addEventListener(FormEvents::POST_SUBMIT, function(FormEvent $formEvent){
+                $user = $formEvent->getData();
+
+                if($formEvent->getForm()->get('super_admin')->getData() === true){
+                    $user->addRole(User::ROLE_SUPER_ADMIN);
+                }else{
+                    $user->removeRole(User::ROLE_SUPER_ADMIN);
+                }
+            });
+        }else{
+            // Only super admin can grant super_admin role
+            $formMapper->remove('super_admin');
+        }
+    }
+
+    /**
+     * Validator to check if we are not creating a situation where there would be no super admin left
+     *
+     * @param $value
+     * @param ExecutionContextInterface $context
+     * @throws \Doctrine\ORM\NonUniqueResultException
+     */
+    public function checkSuperAdminSafe($value, ExecutionContextInterface $context)
+    {
+        if($value !== false || !$this->getSubject()->hasRole(User::ROLE_SUPER_ADMIN)){
+            return;
+        }
+
+        $dql = "SELECT COUNT(u) FROM ".User::class." u WHERE u.roles LIKE :role AND u.id != :user_id";
+        $nbSuperAdmin = $this->doctrine->createQuery($dql)
+            ->setParameter(':user_id', $this->getSubject()->getId())
+            ->setParameter(':role', '%"'.User::ROLE_SUPER_ADMIN.'"%')
+            ->getSingleScalarResult();
+
+        if($nbSuperAdmin === 0){
+            $context->buildViolation('no_super_admin_left')
+                ->atPath('super_admin')
+                ->addViolation();
+        }
     }
 
     /**
